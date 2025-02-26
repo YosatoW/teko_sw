6e2d3b53739ac192bd0b500149838b001b5411ce
YosatoW <wy.onacc@hotmail.com>
2025-02-26 19:21:34 +0100

    Remove merge conflict markers and clean up server initialization logic

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 
-<<<<<<<<< Temporary merge branch 1
-import {initializeAPI} from './api'
-import { initializeMessageBroker } from './message-broker' // neue Zeile
-
-=========
 import { initializeAPI } from './api'
 import { initializeMessageBroker } from './message-broker'
 import { initializeCache } from './services/cache'
->>>>>>>>> Temporary merge branch 2
 
 // Server initialisieren
 export const SERVER_ROLE = process.env.SERVER_ROLE || 'all'
@@ -20,18 +14,8 @@ if (!allowedServerRoles.includes(SERVER_ROLE)) {
 
 // for the worker server & api queue
 initializeMessageBroker()
+initializeCache()
 
-<<<<<<<<< Temporary merge branch 1
-initializeAPI(app)
-
-if (process.env.SERVER_ROLE !== 'worker') {
-  initializeMessageBroker();
-}
-
-
-// initializeMessageBroker()  // neue Zeile
-
-=========
 // For the API server
 if (SERVER_ROLE === 'all' || SERVER_ROLE === 'api') {
     const port = 3000
@@ -41,15 +25,10 @@ if (SERVER_ROLE === 'all' || SERVER_ROLE === 'api') {
     app.use(express.json())
     
     initializeAPI(app)
-    initializeCache()
->>>>>>>>> Temporary merge branch 2
+    
 
     // Server starten
     app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-<<<<<<<<< Temporary merge branch 1
-})
-=========
     })
 }
->>>>>>>>> Temporary merge branch 2
