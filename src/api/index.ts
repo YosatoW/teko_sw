82bda3edbee1273c53bb4acb2811e98e4f9e3dba
Chvvn17 <minh-chien.luong@hotmail.com>
2025-03-03 09:18:43 +0100

    AB12_Tests

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,11 @@ import type {Express} from 'express'
 import {initializePostsAPI} from './posts'
 import { initializeAuthAPI } from './auth'
 import authMiddleware from './auth-middleware'
+import {limiter} from './rate-limiter'
 
 export const initializeAPI = (app: Express) => {
     app.use(authMiddleware)
+    app.use(limiter)
     initializePostsAPI(app)
     initializeAuthAPI(app)
 }
