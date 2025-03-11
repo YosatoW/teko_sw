002ffe4f9716b04e5a0e02316b1399b671516d9c
Chvvn17 <minh-chien.luong@hotmail.com>
2025-03-11 21:43:53 +0100

    AB13_Bis2b

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,13 @@ import {initializePostsAPI} from './posts'
 import { initializeAuthAPI } from './auth'
 import authMiddleware from './auth-middleware'
 import {limiter} from './rate-limiter'
+import { httpLogger } from '../services/logger';
 
 export const initializeAPI = (app: Express) => {
-    app.use(authMiddleware)
-    app.use(limiter)
-    initializePostsAPI(app)
-    initializeAuthAPI(app)
-}
+    app.use(authMiddleware); // Auth-Middleware zuerst
+    app.use(httpLogger); // Logger-Middleware hinzufügen
+    app.use(limiter); // Rate-Limiter danach
+
+    initializePostsAPI(app);
+    initializeAuthAPI(app);
+};
