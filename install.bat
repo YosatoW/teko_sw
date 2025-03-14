7cbca9b0442205df07caf5f79ac639b73ded5235
YosatoW <wy.onacc@hotmail.com>
2025-03-14 09:37:55 +0100

    Refactor installation script and add performance evaluation documentation

--- install.bat
+++ install.bat
@@ -2,13 +2,9 @@
 echo Starte Bun-Installation...
 
 bun install
-bun update
 
-bun add drizzle-orm pg dotenv
-bun add -D drizzle-kit tsx @types/pg
 
 bun install --cwd ./frontend
-bun update --cwd ./frontend
 
 echo Fertig!
 pause
