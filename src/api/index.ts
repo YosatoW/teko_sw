1fe584d6bdd2fe218cddc6f7355f024a753f8907
YosatoW <wy.onacc@hotmail.com>
2025-03-18 16:43:29 +0100

    Refactor configuration files and improve API initialization order

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,21 +7,21 @@ import {limiter} from './rate-limiter'
 import { httpLogger } from '../services/logger';
 
 export const initializeAPI = (app: Express) => {
-    // Prometheus Middleware zuerst hinzufügen
-    app.use(
-      promMid({
-        metricsPath: '/metrics',
-        collectDefaultMetrics: false, // Muss auf false sein
-        requestDurationBuckets: [0.1, 0.5, 1, 1.5],
-        requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-        responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-      })
-    );
-    
-    app.use(authMiddleware); // Auth-Middleware zuerst
-    app.use(httpLogger); // Logger-Middleware hinzufügen
-    app.use(limiter); // Rate-Limiter danach
+  // Prometheus Middleware zuerst hinzufügen
+  app.use(
+    promMid({
+      metricsPath: '/metrics',
+      collectDefaultMetrics: false, // Muss auf false sein
+      requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+      requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+      responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    })
+  );
   
-    initializePostsAPI(app);
-    initializeAuthAPI(app);
-  };
+  app.use(authMiddleware); // Auth-Middleware zuerst
+  app.use(httpLogger); // Logger-Middleware hinzufügen
+  app.use(limiter); // Rate-Limiter danach
+
+  initializePostsAPI(app);
+  initializeAuthAPI(app);
+};
